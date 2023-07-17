c2b1ba395acb1cae2c9e1a0ef68bc6a20533da69
kdb1217 <mokdb17@gmail.com>
2023-07-17 11:56:45 +0900

    [Setting] Project Foldering(#3)

--- Dots/Dots/DotsApp.swift
+++ Dots/Dots/DotsApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @main
 struct DotsApp: App {
-    let persistenceController = PersistenceController.shared
+//    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
