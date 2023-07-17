06ce01e98bd23d255c46c4690a626fc34aa2fddd
kdb1217 <mokdb17@gmail.com>
2023-07-17 09:51:04 +0900

    [Setting] Project Foldering(#3)

---
+++ Dots/Dots/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Dots
+//
+//  Created by 김다빈 on 2023/07/17.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
