0fe3cbfda300d4cf2dde7799ae6768406585f92e
ChaeeunShin <shince0805@gmail.com>
2023-07-19 15:46:48 +0900

    [ADD] ConnectionProfileModal 추가

---
+++ Dots/Dots/View/Modals/ConnectionProfileModal.swift
@@ -0,0 +1,20 @@
+//
+//  ConnectionProfileModal.swift
+//  Dots
+//
+//  Created by Chaeeun Shin on 2023/07/19.
+//
+
+import SwiftUI
+
+struct ConnectionProfileModal: View {
+    var body: some View {
+        Text("인맥 프로필이 나타나는 모달입니다.")
+    }
+}
+
+struct ConnectionProfileModal_Previews: PreviewProvider {
+    static var previews: some View {
+        ConnectionProfileModal()
+    }
+}
