d434c86830e4ca220fc2616148d73a70bb7ce8ba
kdb1217 <mokdb17@gmail.com>
2023-07-19 20:34:31 +0900

    [FEAT]  리스트에서 리스트를 클릭하면 자동을 TextField에 입력기능 #22

--- Dots/Dots/View/Modals/AddStrengthModal.swift
+++ Dots/Dots/View/Modals/AddStrengthModal.swift
@@ -8,15 +8,18 @@
 import SwiftUI
 
 struct AddStrengthModal: View {
+    var samples: [String] = ["UIkit","SwiftUI","UI구현","샘플입니다","검색어에안떠야해요"]
     @EnvironmentObject var dotsModel: DotsModel
     @Binding var strengthName : String
     @Binding var pagenum: Int
     @State var isError: Bool = false
+    @State var isKeyboardVisible = false
+    @State var tappedNum = 0
     @Environment(\.presentationMode) var presentation
     
     var body: some View {
         
-        VStack(alignment: .center){
+        VStack(alignment: .center) {
             HStack{
                 Text("강점 추가")
                     .font(.system(.title3))
@@ -28,8 +31,8 @@ struct AddStrengthModal: View {
                 .frame(height: 56)
                 .shadow(radius: 2)
                 .foregroundColor(.white)
-                .overlay(){
-                    HStack{
+                .overlay() {
+                    HStack {
                         Button {
                             print("검색기능 예정")
                         } label: {
@@ -43,6 +46,11 @@ struct AddStrengthModal: View {
                                     strengthName = String(strengthName.prefix(20))
                                 }
                             }
+//                            .overlay(
+//                                RoundedRectangle(cornerRadius: 12)
+//                                    .foregroundColor(.blue)
+//                                    .frame(height: 40)
+//                            )
                         Spacer()
                         Text("\(strengthName.count)/20")
                         Button {
@@ -57,22 +65,63 @@ struct AddStrengthModal: View {
                         
                     }
                 }
-            
-            HStack{
-                Spacer()
-                SelectBtn(fontWeight: .regular, content: "이전", textColor: .gray, btnColor: .accentColor, action:{ presentation.wrappedValue.dismiss()})
-                SelectBtn(fontWeight: .bold, content: "다음", textColor: .white, btnColor: .blue,action: {
-                    if dotsModel.addStrength(name: strengthName) == .redunant {
-                        isError = true
-                    } else {
-                        pagenum += 1
+            if isKeyboardVisible {
+                ZStack(alignment: .topLeading) {
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.gray,lineWidth: 0.5)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 140)
+                    VStack {
+                        ScrollView {
+                            
+                            ForEach(samples.indices, id: \.self) { i in
+                                StrengthList(strength: samples[i], strengthName: $strengthName)
+                                
+                            }
+                            Button {
+                            } label: {
+                                Text("\(Image(systemName: "plus.circle.fill")) 직접 추가하기")
+                                    .modifier(regularCallout(colorName: Fontcolor.fontGray.colorName))
+                            }
+                            
+                        }
+                     
                     }
-                })
-                Spacer()
+                    .padding(.top,18)
+                    
+                }
             }
-            .frame(maxWidth: .infinity)
             
+            else {
+                HStack {
+                    Spacer()
+                    SelectBtn(fontWeight: .regular, content: "이전", textColor: .gray, btnColor: .accentColor, action:{ presentation.wrappedValue.dismiss()})
+                    SelectBtn(fontWeight: .bold, content: "다음", textColor: .white, btnColor: .blue,action: {
+                        if dotsModel.addStrength(name: strengthName) == .redunant {
+                            isError = true
+                        } else {
+                            pagenum += 1
+                        }
+                    })
+                    .disabled(strengthName.isEmpty)
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+                
+            }
+        }
+        .onAppear {
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
+                isKeyboardVisible = true
+            }
             
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) {
+                Notification in isKeyboardVisible = false
+            }
+        }
+        .onDisappear {
+            NotificationCenter.default.removeObserver(self)
         }
         .interactiveDismissDisabled()
         .allowsTightening(true)
@@ -85,6 +134,6 @@ struct AddStrengthModal: View {
             }
         } message: {
             Text("이미 나의 강점에 추가된 강점이거나, 존재하는 강점입니다.")
-        }        
+        }
     }
 }
