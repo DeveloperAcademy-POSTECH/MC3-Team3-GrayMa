6b39a74ec42b68c5cb8b3d24b350615e2528231e
ChaeeunShin <shince0805@gmail.com>
2023-07-19 11:47:28 +0900

    [ADD] ConnectionDetailView 생성

---
+++ Dots/Dots/View/ConnectionDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ConnectionDetailView.swift
+//  Dots
+//
+//  Created by Chaeeun Shin on 2023/07/19.
+//
+
+import SwiftUI
+
+struct ConnectionDetailView: View {
+    var body: some View {
+        Text("인맥 디테일 뷰입니다.")
+    }
+}
+
+struct ConnectionDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConnectionDetailView()
+    }
+}
