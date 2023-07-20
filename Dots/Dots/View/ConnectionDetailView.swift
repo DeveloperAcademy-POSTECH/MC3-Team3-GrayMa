1754d4c42a7ae2cde709ee17b77d818c79469c93
ChaeeunShin <shince0805@gmail.com>
2023-07-20 11:14:40 +0900

    [ADD] 인맥 개인 디테일뷰 추가

---
+++ Dots/Dots/View/ConnectionDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ConnectionDetailView.swift
+//  Dots
+//
+//  Created by Chaeeun Shin on 2023/07/20.
+//
+
+import SwiftUI
+
+struct ConnectionDetailView: View {
+    var body: some View {
+        Text("인맥 개인 프로필 뷰")
+    }
+}
+
+struct ConnectionDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConnectionDetailView()
+    }
+}
