f30114667bb0b2146761ade8c470db552fd13643
ChaeeunShin <shince0805@gmail.com>
2023-07-20 11:17:16 +0900

    [ADD] 인맥 개인 프로필 모달 추가(#40)

---
+++ Dots/Dots/View/Modals/ConnectionProfileModal.swift
@@ -0,0 +1,20 @@
+//
+//  ConnectionProfileModal.swift
+//  Dots
+//
+//  Created by Chaeeun Shin on 2023/07/20.
+//
+
+import SwiftUI
+
+struct ConnectionProfileModal: View {
+    var body: some View {
+        Text("프로필 모달 뷰")
+    }
+}
+
+struct ConnectionProfileModal_Previews: PreviewProvider {
+    static var previews: some View {
+        ConnectionProfileModal()
+    }
+}
