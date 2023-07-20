1560dbfc943ff1521c0eb69542527b88ecc43b55
kdb1217 <mokdb17@gmail.com>
2023-07-20 17:40:07 +0900

    [FEAT] 왼쪽으로 스와이프하면 delete버튼이 나오게 하기 위한 구조체 파일 생성 (#46)

---
+++ Dots/Dots/View/Component/SwipeItemView.swift
@@ -0,0 +1,20 @@
+//
+//  SwipeItemView.swift
+//  Dots
+//
+//  Created by 김다빈 on 2023/07/20.
+//
+
+import SwiftUI
+
+struct SwipeItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwipeItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwipeItemView()
+    }
+}
