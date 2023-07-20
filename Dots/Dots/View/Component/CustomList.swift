c055a0b9b7e4b86f054c83ff1117782a9432a3e4
kdb1217 <mokdb17@gmail.com>
2023-07-20 20:38:52 +0900

    [FEAT] 커스텀 리스트 초기 버전 작성 (46)

--- Dots/Dots/View/Component/CustomList.swift
+++ Dots/Dots/View/Component/CustomList.swift
@@ -4,6 +4,34 @@
 //
 //  Created by 김다빈 on 2023/07/19.
 //
+// MARK: 이전 커스텀 리스트
+//        RoundedRectangle(cornerRadius: 12)
+//            .foregroundColor(.white)
+//            .frame(maxWidth: .infinity)
+//            .frame(height: 84)
+//
+//            .shadow(radius: 2)
+//            .overlay()
+//        {
+//            HStack(alignment: .center){
+//                Image(StrengthLevelImage.allCases[Int(entity.strengthLevel)].rawValue)
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 36,height: 36)
+//                    .padding(.leading,22)
+//                Text(entity.ownStrength?.strengthName ?? "이름")
+//                    .font(.system(size: 17,weight: .semibold))
+//                    .padding(.leading,12)
+//                Spacer()
+//                NavigationLink(destination: EmptyView()) {
+//                    Text("\(Image(systemName: "chevron.right"))")
+//                        .font(.system(size: 15, weight: .regular))
+//                        .padding(.trailing, 14)
+//                }
+//            }
+//        }
+//        .padding(.top, 10)
+//        .padding(.horizontal,16)
 
 import SwiftUI
 
@@ -17,34 +45,50 @@ struct CustomList: View {
     var entity: MyStrengthEntity
     
     var body: some View {
-        RoundedRectangle(cornerRadius: 12)
-            .foregroundColor(.white)
-            .frame(maxWidth: .infinity)
-            .frame(height: 84)
-          
-            .shadow(radius: 2)
-            .overlay()
-        {
-            HStack(alignment: .center){
-                Image(StrengthLevelImage.allCases[Int(entity.strengthLevel)].rawValue)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 36,height: 36)
-                    .padding(.leading,22)
-                Text(entity.ownStrength?.strengthName ?? "이름")
-                    .font(.system(size: 17,weight: .semibold))
-                    .padding(.leading,12)
-                Spacer()
-                NavigationLink(destination: EmptyView()) {
-                    Text("\(Image(systemName: "chevron.right"))")
-                        .font(.system(size: 15, weight: .regular))
-                        .padding(.trailing, 14)
+        SwipeItemView(content: {
+            Button {
+                print("버튼인척")
+            } label: {
+                HStack(alignment: .center){
+                    Image(StrengthLevelImage.allCases[Int(entity.strengthLevel)].rawValue)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 36,height: 36)
+                        .padding(.leading,22)
+                    Text(entity.ownStrength?.strengthName ?? "이름")
+                        .font(.system(size: 17,weight: .semibold))
+                        .padding(.leading,12)
+                    Spacer()
+                    NavigationLink(destination: EmptyView()) {
+                        Text("\(Image(systemName: "chevron.right"))")
+                            .font(.system(size: 15, weight: .regular))
+                            .padding(.trailing, 14)
+                    }
                 }
             }
-        }
-        .padding(.top, 10)
-        .padding(.horizontal,16)
-       
+        }, right: {
+            VStack {
+                Button(action: {
+                    print("삭제예정")
+                }, label: {
+                    ZStack {
+                        Image(systemName: "trash")
+                            .resizable()
+                            .font(.system(size: 17))
+                            .foregroundColor(.red)
+                    }
+                })
+                .buttonStyle(PlainButtonStyle())
+                .cornerRadius(10)
+
+            }
+            .padding(.trailing, 20)
+        }, itemHeight: 84)
+
+//        func delete() {
+//            //디비 삭제 예정
+//            return
+//        }
         
     }
 }
--- Dots/Dots/View/Component/SwipeItemView.swift
+++ Dots/Dots/View/Component/SwipeItemView.swift
@@ -7,14 +7,75 @@
 
 import SwiftUI
 
-struct SwipeItemView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct SwipeItemView<Content: View, Right: View>: View {
+    var content: () -> Content
+    var right: () -> Right
+    var itemHeight: CGFloat
+    
+    init(content: @escaping () -> Content, right: @escaping () -> Right, itemHeight: CGFloat) {
+        self.content = content
+        self.right = right
+        self.itemHeight = itemHeight
     }
-}
-
-struct SwipeItemView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwipeItemView()
+    
+    @State var hoffSet: CGFloat = 0
+    @State var anchor: CGFloat = 0
+    
+    let screenWidth = UIScreen.main.bounds.width
+    var anchorWidth: CGFloat { screenWidth / 3}
+    var swipeTreshold: CGFloat { screenWidth / 15 }
+    
+    @State var rightPast = false
+    
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                withAnimation {
+                    hoffSet = anchor + value.translation.width
+                    
+                    if abs(hoffSet) > anchorWidth {
+                        if rightPast {
+                            hoffSet = -anchorWidth
+                        }
+                    }
+                    
+                    if anchor < 0 {
+                        rightPast = hoffSet < -anchorWidth + swipeTreshold
+                    } else {
+                        rightPast = hoffSet < -swipeTreshold
+                    }
+                }
+            }
+            .onEnded { value in
+                withAnimation {
+                    if rightPast {
+                        anchor = -anchorWidth
+                    } else {
+                        anchor = 0
+                    }
+                    hoffSet = anchor
+        
+                }
+                
+            }
+    }
+    
+    var body: some View {
+        GeometryReader { geo in
+            HStack(spacing: 0) {
+                content()
+                    .frame(width: geo.size.width + 20)
+                right()
+                    .frame(width: anchorWidth)
+                    .zIndex(1)
+                    .clipped()
+            }
+            .offset(x: hoffSet)
+            .frame(maxHeight: 84)
+            .contentShape(RoundedRectangle(cornerRadius: 12))
+            .gesture(drag)
+            .clipped()
+            
+        }
     }
 }
