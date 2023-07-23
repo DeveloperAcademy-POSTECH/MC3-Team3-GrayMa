ae815673eb4219998d4d68f1a2d75be34779be75
ChaeeunShin <shince0805@gmail.com>
2023-07-23 15:25:36 +0900

    [ADD] 인맥에 관한 메모를 작성하는 ConnectionNoteModal 추가(#55)

---
+++ Dots/Dots/View/Modals/ConnectionNoteModal.swift
@@ -0,0 +1,20 @@
+//
+//  ConnectionNoteModal.swift
+//  Dots
+//
+//  Created by Chaeeun Shin on 2023/07/23.
+//
+
+import SwiftUI
+
+struct ConnectionNoteModal: View {
+    var body: some View {
+        Text("인맥에 관한 메모를 작성하는 모달입니다.")
+    }
+}
+
+struct ConnectionNoteModal_Previews: PreviewProvider {
+    static var previews: some View {
+        ConnectionNoteModal()
+    }
+}
