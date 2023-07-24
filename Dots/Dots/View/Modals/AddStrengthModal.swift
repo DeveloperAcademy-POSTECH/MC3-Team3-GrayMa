b2f8300e7eb41b2c5d91cd2134adf1a12cef99ba
seunggyun-jeong <jskyun98@gmail.com>
2023-07-25 00:55:09 +0900

    [CHORE] 강점 추가 리펙터링

--- Dots/Dots/View/Modals/AddStrengthModal.swift
+++ Dots/Dots/View/Modals/AddStrengthModal.swift
@@ -20,139 +20,17 @@ struct AddStrengthModal: View {
     var body: some View {
         NavigationView {
             VStack(alignment: .center, spacing: 0) {
-                HStack {
-                    Text("강점 추가")
-                        .modifier(semiBoldTitle3(colorName: .theme.gray5Dark))
-                    
-                    Spacer()
-                }
-                .padding(.horizontal, 16)
-                .padding(.top, 23)
-                .padding(.bottom, 31)
+                Title
                 
-                RoundedRectangle(cornerRadius: 40)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 56)
-                    .shadow(radius: 1)
-                    .foregroundColor(.white)
-                    .overlay() {
-                        HStack {
-                            Image(systemName: "magnifyingglass")
-                                .foregroundColor(.black)
-                                .padding(.leading,14)
-                            ZStack {
-                                TextField("예) 디자인 시스템", text: $strengthName)
-                                    .onChange(of: strengthName) {
-                                        newvalue in if strengthName.count > 20 {
-                                            strengthName = String(strengthName.prefix(20))
-                                        }
-                                    }
-                                    .onSubmit {
-                                        isKeyboardVisible = false
-                                        selectedStrength = strengthName
-                                        strengthName = " "
-                                    }
-                                    .disabled(!selectedStrength.isEmpty)
-                                
-                                HStack {
-                                    HStack(spacing: 0) {
-                                        Text(selectedStrength)
-                                            .modifier(regularBody(colorName: .theme.text))
-                                            .padding(.trailing, 10)
-                                        Button {
-                                            withAnimation(.easeIn(duration: 0.1)) {
-                                                selectedStrength = ""
-                                                strengthName = ""
-                                            }
-                                        } label: {
-                                            Image(systemName: "x.circle.fill")
-                                                .foregroundColor(.theme.disabled)
-                                        }
-                                    }
-                                    .padding(.horizontal, 18)
-                                    .padding(.vertical, 9)
-                                    .background(Color.theme.secondary)
-                                    .cornerRadius(12, corners: .allCorners)
-                                    .opacity(selectedStrength.isEmpty ? 0 : 1)
-                                    
-                                    Spacer()
-                                }
-                            }
-                            
-                            Spacer()
-                            
-                            Text("\(strengthName.count)/20")
-                                .modifier(regularCallout(colorName: .theme.gray))
-                            
-                            Button {
-                                strengthName = ""
-                            } label: {
-                                Text("\(Image(systemName: "x.circle.fill"))")
-                                    .foregroundColor(.theme.disabled)
-                                    .frame(width: 24,height: 24)
-                                    .padding(.trailing,15)
-                            }
-                        }
-                    }
+                SearchBar
                 
                 Spacer()
                 
                 // 키보드가 나타났을때에는 ScrollView가 나타남
                 if isKeyboardVisible {
-                    ZStack(alignment: .center) {
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color.gray,lineWidth:1)
-                            .foregroundColor(.white)
-                        // db에 있는 값들을 리스트로 띄움
-                        ScrollView {
-                            //sample에 filter를 걸어서 포함 되는 단어만 띄우는 반복문 만약 필터에 해당 되는 것이 없다면 직접 입력하라고 리턴
-                            if dotsModel.strength.filter({ $0.strengthName!.contains(strengthName) || strengthName.isEmpty }).isEmpty {
-                                HStack {
-                                    Button {
-                                        _ = dotsModel.addStrength(name: strengthName)
-                                        isKeyboardVisible = false
-                                    } label: {
-                                        Label("직접 추가하기", systemImage: "plus.circle.fill")
-                                            .padding(.top, 18)
-                                            .padding(.leading, 44)
-                                            .modifier(regularCallout(colorName: .theme.gray))
-                                    }
-                                    
-                                    Spacer()
-                                }
-                            } else {
-                                VStack(spacing: 0) {
-                                    ForEach(dotsModel.strength.filter{ $0.strengthName!.contains(strengthName) || strengthName == "" }, id:\.self) {
-                                        filteredStrength in
-                                        StrengthList(strength: filteredStrength.strengthName!, strengthName: $strengthName)
-                                    }
-                                }
-                            }
-                            
-                        }
-                        .clipShape(RoundedRectangle(cornerRadius: 12))
-                    }
-                    .padding(.top, 10)
-                    .frame(height: 200)
+                    ExistStrengthList
                 } else {
-                    VStack {
-                        HStack {
-                            SelectBtn(fontWeight: .regular, content: "취소", textColor: .gray, btnColor: .theme.bgBlank, action:{ presentation.wrappedValue.dismiss()})
-                            SelectBtn(fontWeight: .bold, content: "다음", textColor: .white, btnColor: .accentColor, action: {
-                                if dotsModel.addStrength(name: strengthName) == .redunant {
-                                    isError = true
-                                } else {
-                                    pagenum += 1
-                                }
-                            })
-                            .disabled(selectedStrength.isEmpty)
-                        }
-                        .frame(height: 50)
-                        .frame(maxWidth: .infinity)
-                        Spacer()
-                            .frame(height: 50)
-                        
-                    }
+                    Buttons
                 }
                 
                 Spacer()
@@ -180,6 +58,146 @@ struct AddStrengthModal: View {
             } message: {
                 Text("이미 나의 강점에 추가된 강점이거나, 존재하는 강점입니다.")
             }
+        }
+    }
+}
+
+extension AddStrengthModal {
+    private var Title: some View {
+        HStack {
+            Text("강점 추가")
+                .modifier(semiBoldTitle3(colorName: .theme.gray5Dark))
+            
+            Spacer()
+        }
+        .padding(.horizontal, 16)
+        .padding(.top, 23)
+        .padding(.bottom, 31)
+    }
+    
+    private var SearchBar: some View {
+        RoundedRectangle(cornerRadius: 40)
+            .frame(maxWidth: .infinity)
+            .frame(height: 56)
+            .shadow(radius: 1)
+            .foregroundColor(.white)
+            .overlay() {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.black)
+                        .padding(.leading,14)
+                    ZStack {
+                        TextField("예) 디자인 시스템", text: $strengthName)
+                            .onChange(of: strengthName) {
+                                newvalue in if strengthName.count > 20 {
+                                    strengthName = String(strengthName.prefix(20))
+                                }
+                            }
+                            .onSubmit {
+                                isKeyboardVisible = false
+                                selectedStrength = strengthName
+                                strengthName = " "
+                            }
+                            .disabled(!selectedStrength.isEmpty)
+                        
+                        HStack {
+                            HStack(spacing: 0) {
+                                Text(selectedStrength)
+                                    .modifier(regularBody(colorName: .theme.text))
+                                    .padding(.trailing, 10)
+                                Button {
+                                    withAnimation(.easeIn(duration: 0.1)) {
+                                        selectedStrength = ""
+                                        strengthName = ""
+                                    }
+                                } label: {
+                                    Image(systemName: "x.circle.fill")
+                                        .foregroundColor(.theme.disabled)
+                                }
+                            }
+                            .padding(.horizontal, 18)
+                            .padding(.vertical, 9)
+                            .background(Color.theme.secondary)
+                            .cornerRadius(12, corners: .allCorners)
+                            .opacity(selectedStrength.isEmpty ? 0 : 1)
+                            
+                            Spacer()
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Text("\(strengthName.count)/20")
+                        .modifier(regularCallout(colorName: .theme.gray))
+                    
+                    Button {
+                        strengthName = ""
+                    } label: {
+                        Text("\(Image(systemName: "x.circle.fill"))")
+                            .foregroundColor(.theme.disabled)
+                            .frame(width: 24,height: 24)
+                            .padding(.trailing,15)
+                    }
+                }
+            }
+    }
+    
+    private var ExistStrengthList: some View {
+        ZStack(alignment: .center) {
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.gray,lineWidth:1)
+                .foregroundColor(.white)
+            // db에 있는 값들을 리스트로 띄움
+            ScrollView {
+                //sample에 filter를 걸어서 포함 되는 단어만 띄우는 반복문 만약 필터에 해당 되는 것이 없다면 직접 입력하라고 리턴
+                if dotsModel.strength.filter({ $0.strengthName!.contains(strengthName) || strengthName.isEmpty }).isEmpty {
+                    HStack {
+                        Button {
+                            _ = dotsModel.addStrength(name: strengthName)
+                            isKeyboardVisible = false
+                        } label: {
+                            Label("직접 추가하기", systemImage: "plus.circle.fill")
+                                .padding(.top, 18)
+                                .padding(.leading, 44)
+                                .modifier(regularCallout(colorName: .theme.gray))
+                        }
+                        
+                        Spacer()
+                    }
+                } else {
+                    VStack(spacing: 0) {
+                        ForEach(dotsModel.strength.filter{ $0.strengthName!.contains(strengthName) || strengthName == "" }, id:\.self) {
+                            filteredStrength in
+                            StrengthList(strength: filteredStrength.strengthName!, strengthName: $strengthName)
+                        }
+                    }
+                }
+                
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+        }
+        .padding(.top, 10)
+        .frame(height: 200)
+    }
+    
+    private var Buttons: some View {
+        VStack {
+            HStack {
+                SelectBtn(fontWeight: .regular, content: "취소", textColor: .gray, btnColor: .theme.bgBlank, action:{ presentation.wrappedValue.dismiss()})
+                SelectBtn(fontWeight: .bold, content: "다음", textColor: .white, btnColor: .accentColor, action: {
+                    if dotsModel.addStrength(name: strengthName) == .redunant {
+                        isError = true
+                    } else {
+                        pagenum += 1
+                    }
+                })
+                .disabled(selectedStrength.isEmpty)
+            }
+            .frame(height: 50)
+            .frame(maxWidth: .infinity)
+            Spacer()
+                .frame(height: 50)
+            
         }
     }
 }
