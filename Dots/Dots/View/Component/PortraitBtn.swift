a65c75ddeb1ad66c3abaace1afa0e5d1bc9a2d46
Jae Ho Yoon (Marcus) <mr.jaeho@gmail.com>
2023-07-26 01:40:05 +0900

    [DEL] Portrait 사진 보는 ui 삭제

--- Dots/Dots/View/Component/PortraitBtn.swift
+++ Dots/Dots/View/Component/PortraitBtn.swift
@@ -13,9 +13,12 @@ struct DummyPortrait: Hashable {
 }
 
 var dummyPortraitData: [DummyPortrait] = [
-    DummyPortrait(name: "마커스", portraitName: "testPortrait"),
+    DummyPortrait(name: "유빈"),
+    DummyPortrait(name: "마커스"),
     DummyPortrait(name: "래쉬"),
-    DummyPortrait(name: "유빈", portraitName: "testPortrait")
+    DummyPortrait(name: "규니"),
+    DummyPortrait(name: "앤디"),
+    DummyPortrait(name: "애쉬")
 ]
 
 struct PortraitBtn: View {
@@ -26,36 +29,26 @@ struct PortraitBtn: View {
             Circle()
                 .fill(
                     LinearGradient(
-                      stops: [
-                        Gradient.Stop(color: Color(red: 0.87, green: 0.29, blue: 0.28), location: 0.00),
-                        Gradient.Stop(color: Color(red: 0.96, green: 0.93, blue: 0.86), location: 1.00),
-                      ],
-                      startPoint: UnitPoint(x: 0.5, y: -0.18),
-                      endPoint: UnitPoint(x: 0.5, y: 1.46)
+                        stops: [
+                            Gradient.Stop(color: Color(red: 0.87, green: 0.29, blue: 0.28), location: 0.00),
+                            Gradient.Stop(color: Color(red: 0.96, green: 0.93, blue: 0.86), location: 1.00),
+                        ],
+                        startPoint: UnitPoint(x: 0.5, y: -0.18),
+                        endPoint: UnitPoint(x: 0.5, y: 1.46)
                     ))
-                .frame(width: 80)
+                .frame(width: 76)
             Button(action: {
                 // 인맥 페이지로 들어가기
                 
             }, label: {
                 ZStack {
-                    if let portraitName = entity.portraitName {
-                        Image(portraitName)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 64, height: 64)
-                            .clipShape(Circle())
-                    } else {
-                        ZStack {
-                            Circle()
-                                .frame(width: 64)
-                                .foregroundColor(Color.theme.secondary)
-                            Text(entity.name)
-                                .modifier(boldTitle2(colorName: .theme.text))
-                                .frame(width: 64)
-                                .scaledToFit()
-                        }
-                    }
+                    Circle()
+                        .frame(width: 64)
+                        .foregroundColor(Color.theme.secondary)
+                    Text(entity.name)
+                        .modifier(boldTitle2(colorName: .theme.text))
+                        .frame(width: 64)
+                        .scaledToFit()
                 }
             })
         }
