b6c32e4439e66209b0a779d78c3221f336df10da
seunggyun-jeong <jskyun98@gmail.com>
2023-07-27 23:32:51 +0900

    [FIX] 인맥 랜덤 삭제되는 이슈 해결

--- Dots/Dots/View/SearchView/Modal/ContactsListRow.swift
+++ Dots/Dots/View/SearchView/Modal/ContactsListRow.swift
@@ -66,7 +66,7 @@ struct ContactsListRow: View {
                 }
                 .foregroundColor(.theme.alertRed)
             }, message: {
-                Text("이 사람과 관련된 모든 정보가 삭제됩니다.")
+                Text("\(person.name ?? "")와(과) 관련된 모든 정보가 삭제됩니다.")
             })
     }
 }
