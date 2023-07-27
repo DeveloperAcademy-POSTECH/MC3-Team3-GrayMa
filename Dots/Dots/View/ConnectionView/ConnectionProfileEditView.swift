eb09d6869823bce259d3c4d6b36785a9c4e3f668
ChaeeunShin <shince0805@gmail.com>
2023-07-27 11:14:31 +0900

    [ADD] 인맥 프로필 수정을 위한 뷰 추가(#137)

---
+++ Dots/Dots/View/ConnectionView/ConnectionProfileEditView.swift
@@ -0,0 +1,20 @@
+//
+//  ConnectionProfileEditView.swift
+//  Dots
+//
+//  Created by Chaeeun Shin on 2023/07/27.
+//
+
+import SwiftUI
+
+struct ConnectionProfileEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConnectionProfileEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConnectionProfileEditView()
+    }
+}
