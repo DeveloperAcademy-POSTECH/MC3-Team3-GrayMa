6a58bd830ba4e87d90deb5776a74759ecb24bd05
ChaeeunShin <shince0805@gmail.com>
2023-07-27 20:50:34 +0900

    [CHORE] 인맥 프로필 수정 시트에서 네비게이션으로 변경(#137)

--- Dots/Dots/View/ConnectionView/ConnectionProfileEditView.swift
+++ Dots/Dots/View/ConnectionView/ConnectionProfileEditView.swift
@@ -20,6 +20,91 @@ struct ConnectionProfileEditView: View {
     @State private var editedLink: String = ""
     
     var body: some View {
+
+//        VStack{
+//            // 상단 메뉴바
+//            HStack{
+//                Text("\(Image(systemName: "chevron.left")) 인맥관리")
+//                    .font(.system(size: 22))
+//                    .foregroundColor(Color.theme.primary)
+//                    .onTapGesture { dismiss() }
+//
+//                Spacer()
+//                    .frame(width: 248)
+//
+//                Text("완료")
+//                    .font(.system(size: 22))
+//                    .foregroundColor(Color.theme.primary)
+//
+//                    .onTapGesture {
+//                        dotsModel.up(profileImgIdx: coreDataUSerProfileImgIdx, name: coreDataUserName, company: coreaDataUserCompany, job: coreDataUserJob, phoneNum: coreDataUserPhone, email: coreDataUserEmail, snsUrl: coreDataUserSNS /*, userStrength: coreDataUserStrength*/)
+//                        //인맥 추가 알림 띄움
+//                        addAlert = true
+//                    }
+//            }
+//
+//            ScrollView(showsIndicators: false){
+//                //이미지 추가
+//                contactsImageSelect(userName: coreDataUserName, coreDataUserIdx: $coreDataUSerProfileImgIdx)
+//                    .onTapGesture {
+//                        userImgModal = true
+//                    }
+//                    .sheet(isPresented: $userImgModal) {
+//                        ProfileImageModal(userName: coreDataUserName, userImageIdx: $coreDataUSerProfileImgIdx)
+//                            .presentationDetents([.height(UIScreen.main.bounds.height * 0.4)])
+//                    }
+//
+//
+//                VStack (alignment: .leading){
+//
+//                    ContactsNameField(UserInputName: $coreDataUserName, fieldFocus: fieldFocus)
+//
+//                    ContactsCompanyField(UserInputCompany: $coreaDataUserCompany)
+//
+//                    ContactsJobField(UserInputJob: $coreDataUserJob)
+//
+//                    ContactsPhoneField(UserInputPhone: $coreDataUserPhone, fieldFocus: fieldFocus)
+//
+//                    ContactsEmailField(UserInputEmail: $coreDataUserEmail)
+//
+//                    ContactsSNSField(UserInputSNS: $coreDataUserSNS)
+//
+//                    ContactsStrengthField(UserInputStrengthArr: $coreDataUserStrength)
+//
+//                    //선택된 강점 표시 부분
+//                    ForEach(coreDataUserStrength, id: \.self) { Strength in
+//                        Text(Strength)
+//                    }
+//
+//                }
+//            }
+//        }
+//    }
+//
+//    .alert(isPresented: $addAlert) {
+//        Alert(title: Text("알림"),
+//              message: Text("인맥이 추가되었습니다."),
+//              dismissButton: .default(Text("확인"), action: {
+//            // 모달을 닫습니다.
+//            presentationMode.wrappedValue.dismiss()
+//            modalComtrol = false
+//        }))
+//    }
+//    .onAppear{
+//        if !(selectedUserName?.isEmpty ?? true){
+//            let selectedUser = fetchContact(name: selectedUserName ?? "")
+//            for contact in selectedUser {
+//                coreDataUserName = contact.Name
+//
+//                coreaDataUserCompany = contact.Company ?? ""
+//                coreDataUserJob = contact.Job ?? ""
+//
+//                coreDataUserPhone = contact.PhoneNumber ?? ""
+//                coreDataUserEmail = contact.Email ?? ""
+//                //coreDataUserSNS = contact.SNS ?? ""
+//            }
+//        }
+
         VStack {
             if let existingName = person.name,
                let existingCompany = person.company,
@@ -34,13 +119,29 @@ struct ConnectionProfileEditView: View {
                 TextField(existingMail.isEmpty ? "이메일" : existingMail, text: $editedMail)
                 TextField(existingLink.isEmpty ? "링크드인주소" : existingLink, text: $editedLink)
             }
-            
-            Button {
+        }
+        .navigationBarBackButtonHidden()
+        .navigationBarItems(leading: BackButton)
+        .navigationBarItems(trailing: SaveButton)
+    }
+    
+    private var BackButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            HStack(spacing: 3) {
+                Image(systemName: "chevron.backward")
+                Text("프로필")
+            }
+        }
+    }
+    
+    private var SaveButton: some View {
+        Button {
             dotsModel.updateNetworking(id: person.peopleID!, profileImgIdx: Int(person.profileImageIndex), name: editedName, company: editedCompany, job: editedJob, phoneNum: editedPhoneNum, email: editedMail, snsUrl: editedLink)
                 dismiss()
-            } label: {
-                Text("저장")
-            }
+        } label: {
+            Text("저장")
         }
     }
 }
