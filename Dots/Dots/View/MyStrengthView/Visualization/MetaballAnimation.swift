b2bc0a176dd0a6116801091b7c7ea2820ec6bd2c
seunggyun-jeong <jskyun98@gmail.com>
2023-07-28 11:08:57 +0900

    [DEL] 필요 없는 코드 삭제

--- Dots/Dots/View/MyStrengthView/Visualization/MetaballAnimation.swift
+++ Dots/Dots/View/MyStrengthView/Visualization/MetaballAnimation.swift
@@ -105,66 +105,6 @@ struct MetaballAnimation: View {
             .blur(radius: 10)
             .opacity(0.9)
     }
-    
-    @ViewBuilder
-    func SingleMetaBall() -> some View {
-        TimelineView(.animation(minimumInterval: 3.6, paused: false)) { _ in
-            Rectangle()
-                .fill(.linearGradient(stops: [
-                    Gradient.Stop(color: Color(red: 0.87, green: 0.29, blue: 0.28), location: 0.00),
-                    Gradient.Stop(color: Color(red: 0.96, green: 0.93, blue: 0.86), location: 1.00),
-                ],
-                                      startPoint: UnitPoint(x: 1.06, y: 0.94),
-                                      endPoint: UnitPoint(x: 0.16, y: 0.09)))
-                .mask {
-                    Canvas { context, size in
-                        context.addFilter(.alphaThreshold(min: 0.5, color: .theme.secondary))
-                        context.addFilter(.blur(radius: 30))
-                        context.drawLayer { ctx in
-                            for index in [1, 2] {
-                                if let resolvedView = context.resolveSymbol(id: index) {
-                                    ctx.draw(resolvedView, at: CGPoint(x: size.width / 2, y: size.height / 2))
-                                }
-                            }
-                        }
-                    } symbols: {
-                        ForEach(1...3, id: \.self) { index in
-                            let offset = (startAnimation ? CGSize(width: .random(in: -180...180), height: .random(in: -240...240)) : .zero)
-                            Ball(offset: startAnimation ? offset : dragOffset)
-                                .tag(index)
-                        }
-                    }
-                }
-        }
-        .gesture(
-            DragGesture()
-                .onChanged({ value in
-                    dragOffset = value.translation
-                    print(dragOffset)
-                    startAnimation = false
-                }).onEnded({ _ in
-                    withAnimation(.interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7)) {
-                        dragOffset = .zero
-                    }
-                    startAnimation = true
-                })
-        )
-    }
-    
-    @ViewBuilder
-    func Ball(offset: CGSize = .zero, ballLevel: StrengthLevelImage = .moderateDot) -> some View {
-        Circle()
-            .fill(.white)
-            .frame(width: ballLevel.ballSize, height: ballLevel.ballSize)
-            .offset(offset)
-            .animation(.easeInOut(duration: 2), value: startAnimation)
-    }
 }
-//
-//struct MetaballAnimation_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MetaballAnimation(myStrength: [.init(context: <#T##NSManagedObjectContext#>)])
-//    }
-//}
 
 
