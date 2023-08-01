6b9d71ac4bb9eca0a947985181be1c416dc3df80
Jae Ho Yoon (Marcus) <mr.jaeho@gmail.com>
2023-08-01 15:03:10 +0900

    [FIX] '저장된 강점이 없습니다' 눌러서 강점 추가

--- Dots/Dots/View/MyStrengthView/MyStrengthView.swift
+++ Dots/Dots/View/MyStrengthView/MyStrengthView.swift
@@ -55,21 +55,23 @@ struct MyStrengthView: View {
                         
                         ScrollView {
                             if dotsModel.myStrength.isEmpty {
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(Color.theme.gray5, lineWidth: 1.5)
-                                    .foregroundColor(.theme.bgPrimary)
-                                    .frame(maxWidth: .infinity)
-                                    .frame(height: 84)
-                                    .overlay() {
-                                        HStack {
-                                            Text("저장된 강점이 없습니다.")
-                                                .modifier(regularBody(colorName: .theme.gray))
-                                                .padding(.leading, 29)
-                                            
-                                            Spacer()
+                                Button(action: { self.showModal = true }) {
+                                    RoundedRectangle(cornerRadius: 12)
+                                        .stroke(Color.theme.gray5, lineWidth: 1.5)
+                                        .foregroundColor(.theme.bgPrimary)
+                                        .frame(maxWidth: .infinity)
+                                        .frame(height: 84)
+                                        .overlay() {
+                                            HStack {
+                                                Text("저장된 강점이 없습니다.")
+                                                    .modifier(regularBody(colorName: .theme.gray))
+                                                    .padding(.leading, 29)
+                                                
+                                                Spacer()
+                                            }
                                         }
-                                    }
-                                    .padding(.horizontal, 16)
+                                }
+                                .padding(.horizontal, 16)
                                 
                             } else {
                                 let sortedMyStrength = getSortMyStrength()
